ce3774f2a350b50aaa6373683c0f00b0e9d13558
Serhiy K <mrlanu@gmail.com>
2018-12-03 22:56:33 -0600

    Operations component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
+import { OperationsComponent } from './operations/operations.component';
+import { IncomeComponent } from './operations/income/income.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OperationsComponent,
+    IncomeComponent,
   ],
   imports: [
     MaterialModule,
