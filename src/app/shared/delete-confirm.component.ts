900ad156c6ac66b96be0c8db48f0cf3aaabd9a05
Serhiy K <mrlanu@gmail.com>
2018-12-20 11:23:13 -0600

    edited DeleteConfirmComponent

--- src/app/shared/delete-confirm.component.ts
+++ src/app/shared/delete-confirm.component.ts
@@ -3,12 +3,15 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-delete-all-tasks',
-  template: `<h1 mat-dialog-title>Do you really want to delete this?</h1>
-            <mat-dialog-content>
+  template: `<mat-dialog-content fxLayout fxLayoutAlign="center">
+              <h1 mat-dialog-title>Do you really want to delete this?</h1>
             </mat-dialog-content>
-            <mat-dialog-actions fxLayout="row" fxLayoutAlign="center" fxLayoutGap="15px">
-              <button mat-raised-button [mat-dialog-close]="true">Yes</button>
-              <button mat-raised-button [mat-dialog-close]="false">No</button>
+            <mat-divider></mat-divider>
+            <mat-dialog-actions fxLayout="row" fxLayoutAlign="center">
+              <div style="padding: 20px" fxLayout="row" fxLayoutAlign="center" fxLayoutGap="15px">
+                <button mat-raised-button color="warn" [mat-dialog-close]="true">Yes</button>
+                <button mat-raised-button color="primary" [mat-dialog-close]="false">No</button>
+              </div>
             </mat-dialog-actions>`
 })
 export class DeleteConfirmComponent {
