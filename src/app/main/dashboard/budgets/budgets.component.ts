2a444e198a6d625382ffdecce824131c32188493
Serhiy K <mrlanu@gmail.com>
2019-01-12 22:59:58 -0600

    Changing the Budget

--- src/app/main/dashboard/budgets/budgets.component.ts
+++ src/app/main/dashboard/budgets/budgets.component.ts
@@ -3,6 +3,9 @@ import {Budget} from '../../../models/budget.model';
 import {AuthService} from '../../../auth/auth.service';
 import {HttpService} from '../../../http.service';
 import {UserInfo} from '../../../models/user-info.model';
+import {environment} from '../../../../environments/environment';
+import {SummaryService} from '../summaries/summary.service';
+import {UiService} from '../../../shared/ui.service';
 
 @Component({
   selector: 'app-budgets',
@@ -14,16 +17,25 @@ export class BudgetsComponent implements OnInit {
   budgets: Budget[] = [];
   users: UserInfo[] = [];
 
-  constructor(private authService: AuthService, private httpService: HttpService) { }
+  constructor(private authService: AuthService,
+              private httpService: HttpService,
+              private summaryService: SummaryService,
+              private uiService: UiService) { }
 
   ngOnInit() {
     this.budgets = this.authService.loggedUser.budgets;
   }
 
-  onBudgetSelect(budgetId: number) {
+  onBudgetOpened(budgetId: number) {
     this.httpService.getUsersByBudgetId(budgetId).subscribe((users: UserInfo[]) => {
       this.users = users;
     });
+  }
+
+  onSelectBudget(budgetId: number) {
+    environment.budgetId = budgetId;
+    this.summaryService.getBrief();
+    this.uiService.openSnackBar('Budget has been selected.', null, 5000);
   }
 
 }
